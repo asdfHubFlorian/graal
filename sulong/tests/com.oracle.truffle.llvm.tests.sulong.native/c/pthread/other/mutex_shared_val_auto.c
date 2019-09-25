e82e15071e72182391ecfcae10f0d7700160d161
Jacob Kreindl <jacob.kreindl@jku.at>
2019-09-25 17:52:15 +0200

    Add Copyright Header to Tests

--- sulong/tests/com.oracle.truffle.llvm.tests.sulong.native/c/pthread/other/mutex_shared_val_auto.c
+++ sulong/tests/com.oracle.truffle.llvm.tests.sulong.native/c/pthread/other/mutex_shared_val_auto.c
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
 // in the output "thread got the mutex" has always to be followed by "thread
 // increased the value" in the next line otherwise some other thread also could
 // aquire the mutex while the other thread did not unlock it yet the output /
